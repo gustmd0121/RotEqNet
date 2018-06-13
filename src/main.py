2bfcc3ab93891e38222d28d2d0f5b1cd4b8b1908
marv0810 <struffert.marvin@gmail.com>
2018-06-13 16:17:29 +0200

    Clear up parser.

--- src/main.py
+++ src/main.py
@@ -1,26 +1,14 @@
 from parser import Parser
 import os
 
-# file = "Allogymnopleuri_#09"
 data_folder = "../data/"
-scale_factor = 0.5
-
-# p = Parser(data_folder)
-# p.parse(file)
-# p.create_numpy_arrays(file, scale_factor)
-# p.generate_input(file, scale_factor)
-# print("Finished.")
-# p.load_image(file, 100)
 
 p = Parser(data_folder)
 for sub_folder in os.listdir(data_folder):
     sub_path = os.path.join(data_folder, sub_folder)
     if os.path.isdir(sub_path):
-        print("Parsing", sub_folder, "...")
-        p.parse(sub_folder)
-        p.create_numpy_arrays(sub_folder, scale_factor, True)
-        p.generate_input(sub_folder, scale_factor, True)
-        print("Finished", sub_folder + ".")
-        print("")
-        # p.load_image(sub_folder, 100)
+        p.create_cropped_files(sub_folder, (540, 960))
+        # p.create_scaled_files(sub_folder, 0.5)
+        # try to open input
+        # p.load_numpy(sub_folder, 100)
 print("Finished.")
