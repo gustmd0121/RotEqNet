05fe642a5890c2a7c954ab61725afbe34b04bf6f
marv0810 <struffert.marvin@gmail.com>
2018-06-14 14:09:27 +0200

    Clean up mnist.

--- src/net.py
+++ src/net.py
@@ -1,5 +1,4 @@
 from __future__ import division, print_function
-# from torchsummary import summary
 import torch
 from torch import cuda
 import  torch.nn as nn
@@ -8,13 +7,18 @@ from torch import optim
 import numpy as np
 from torch.autograd import Variable
 import random
-from .mnist import loadMnistRot, random_rotation, linear_interpolation_2D
+from PIL import Image
 from torchvision import transforms
+#from .mnist import loadMnistRot, random_rotation, linear_interpolation_2D
+#from ..utils import getGrid, rotate_grid_2D
+
+#from mnist import random_rotation, loadMnist
+
 
 import sys
 #sys.path.append('../') #Import
-from ..layers_2D import *
-from ..utils import getGrid
+from .layers_2D import *
+from .utils import getGrid
 
 #!/usr/bin/env python
 __author__ = "Anders U. Waldeland"
@@ -28,6 +32,11 @@ https://arxiv.org/abs/1612.09346
 https://github.com/dmarcosg/RotEqNet
 """
 
+epoch_size = 5
+batch_size = 5
+train_file = "Lamarcki_#09"
+test_file = "Lamarcki_#09"
+base_folder = "./data/"
 img_size = (300, 400)
 
 if __name__ == '__main__':
@@ -125,7 +134,6 @@ if __name__ == '__main__':
 
     if True: #Current best setup using this implementation - error rate of 1.2%
         start_lr = 0.01
-        batch_size = 5
         optimizer = optim.Adam(net.parameters(), lr=start_lr)  # , weight_decay=0.01)
         use_test_time_augmentation = True
         use_train_time_augmentation = True
@@ -234,10 +242,37 @@ if __name__ == '__main__':
             for param_group in optimizer.param_groups:
                 param_group['lr'] = lr
 
+
+    def load_data(train, test):
+        # trainfiles
+        imgs =  np.load(base_folder + train + "/" + train + "_input.npz")['data']
+        imgs = np.split(imgs, imgs.shape[0],0)
+        for i in range(len(imgs)):
+            imgs[i] = imgs[i] / 255 - 0.5
+        mask_data = np.load(base_folder + train + "/" + train + "_masks.npz")
+        mask_data = np.split(mask_data['beetle'], mask_data['beetle'].shape[2],2)
+        # for i in range(len(mask_data)):
+        #     mask_data[i] = np.squeeze(np.stack([mask_data[i], mask_data[i] * -1 + np.ones(mask_data[i].shape)], axis=0))
+        train = list(zip(imgs, mask_data))
+
+
+        # testfiles
+        imgs =  np.load(base_folder + test + "/" + test + "_input.npz")['data']
+        imgs = np.split(imgs, imgs.shape[0],0)
+        for i in range(len(imgs)):
+            imgs[i] = imgs[i] / 255 - 0.5
+        mask_data = np.load(base_folder + test + "/" + test + "_masks.npz")
+        mask_data = np.split(mask_data['beetle'], mask_data['beetle'].shape[2],2)
+        # for i in range(len(mask_data)):
+        #     mask_data[i] = np.squeeze(np.stack([mask_data[i], mask_data[i] * -1 + np.ones(mask_data[i].shape)], axis=0))
+        test = list(zip(imgs, mask_data))
+
+        return train, train, test
+
     #Load datasets
-    train_set, val_set,  test_set = loadMnistRot()
+    train_set, val_set,  test_set = load_data(train_file, test_file)
     best_acc = 0
-    for epoch_no in range(5):
+    for epoch_no in range(epoch_size):
 
         #Random order for each epoch
         train_set_for_epoch = train_set[:] #Make a copy
@@ -287,7 +322,7 @@ if __name__ == '__main__':
     from PIL import Image
 
     loader = transforms.Compose([transforms.ToTensor()])
-    image = Image.fromarray(train_set[50][0][0, :, :])
+    image = Image.fromarray(test_set[50][0][0, :, :])
     image = loader(image).float()
     image = Variable(image, requires_grad=True)
     image = image.unsqueeze(0)  # this is for VGG, may not be needed for ResNet
@@ -296,8 +331,120 @@ if __name__ == '__main__':
     xyz = xyz.data.cpu().numpy()
     xyz = np.squeeze(xyz)
     xyz = Image.fromarray(xyz[:, :]*255)
-    mask = Image.fromarray(train_set[50][1][0, :, :]*255)
-    orig = Image.fromarray(train_set[50][0][0, :, :]*255)
+    mask = Image.fromarray(test_set[50][1][0, :, :]*255)
+    orig = Image.fromarray((test_set[50][0][0, :, :]+0.5)*255)
     print(xyz.show(title='net'))
     print(mask.show(title='mask'))
     print(orig.show(title='orig'))
+
+
+
+# not used ("mnist.py" file)
+# def linear_interpolation_2D(input_array, indices, outside_val=0, boundary_correction=True):
+#     # http://stackoverflow.com/questions/6427276/3d-interpolation-of-numpy-arrays-without-scipy
+#     output = np.empty(indices[0].shape)
+#     ind_0 = indices[0,:]
+#     ind_1 = indices[1,:]
+#
+#     N0, N1 = input_array.shape
+#
+#     x0_0 = ind_0.astype(np.integer)
+#     x1_0 = ind_1.astype(np.integer)
+#     x0_1 = x0_0 + 1
+#     x1_1 = x1_0 + 1
+#
+#     # Check if inds are beyond array boundary:
+#     if boundary_correction:
+#         # put all samples outside datacube to 0
+#         inds_out_of_range = (x0_0 < 0) | (x0_1 < 0) | (x1_0 < 0) | (x1_1 < 0) |  \
+#                             (x0_0 >= N0) | (x0_1 >= N0) | (x1_0 >= N1) | (x1_1 >= N1)
+#
+#         x0_0[inds_out_of_range] = 0
+#         x1_0[inds_out_of_range] = 0
+#         x0_1[inds_out_of_range] = 0
+#         x1_1[inds_out_of_range] = 0
+#
+#     w0 = ind_0 - x0_0
+#     w1 = ind_1 - x1_0
+#     # Replace by this...
+#     # input_array.take(np.array([x0_0, x1_0, x2_0]))
+#     output = (input_array[x0_0, x1_0] * (1 - w0) * (1 - w1)  +
+#               input_array[x0_1, x1_0] * w0 * (1 - w1)  +
+#               input_array[x0_0, x1_1] * (1 - w0) * w1  +
+#               input_array[x0_1, x1_1] * w0 * w1 )
+#
+#
+#     if boundary_correction:
+#         output[inds_out_of_range] = 0
+#
+#     return output
+#
+#
+# def random_rotation(data):
+#     rot = np.random.rand() * 360  # Random rotation
+#     grid = getGrid([28, 28])
+#     grid = rotate_grid_2D(grid, rot)
+#     grid += 13.5
+#     data = linear_interpolation_2D(data, grid)
+#     data = np.reshape(data, [28, 28])
+#     data = data / float(np.max(data))
+#     return data.astype('float32')
+
+# not used ("make_mnist_rot.py" file)
+# def makeMnistRot():
+#     """
+#     Make MNIST-rot from MNIST
+#     Select all training and test samples from MNIST and select 10000 for train,
+#     2000 for val and 50000 for test. Apply a random rotation to each image.
+#
+#     Store in numpy file for fast reading
+#
+#     """
+#     np.random.seed(0)
+#
+#     #Get all samples
+#     all_samples = loadMnist('train') + loadMnist('test')
+#
+#     #
+#
+#     #Empty arrays
+#     train_data = np.zeros([28,28,10000])
+#     train_label = np.zeros([10000])
+#     val_data = np.zeros([28,28,2000])
+#     val_label = np.zeros([2000])
+#     test_data = np.zeros([28,28,50000])
+#     test_label = np.zeros([50000])
+#
+#     i = 0
+#     for j in range(10000):
+#         sample =all_samples[i]
+#         train_data[:, :, j] =  random_rotation(sample[0])
+#         train_label[j] = sample[1]
+#         i += 1
+#
+#     for j in range(2000):
+#         sample = all_samples[i]
+#         val_data[:, :, j] = random_rotation(sample[0])
+#         val_label[j] = sample[1]
+#         i += 1
+#
+#     for j in range(50000):
+#         sample = all_samples[i]
+#         test_data[:, :, j] = random_rotation(sample[0])
+#         test_label[j] = sample[1]
+#         i += 1
+#
+#
+#     try:
+#         os.mkdir('mnist_rot/')
+#     except:
+#         None
+#     np.save('mnist_rot/train_data',train_data)
+#     np.save('mnist_rot/train_label', train_label)
+#     np.save('mnist_rot/val_data', val_data)
+#     np.save('mnist_rot/val_label', val_label)
+#     np.save('mnist_rot/test_data', test_data)
+#     np.save('mnist_rot/test_label', test_label)
+#
+# if __name__ == '__main__':
+#     makeMnistRot()
